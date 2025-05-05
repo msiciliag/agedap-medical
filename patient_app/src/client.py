c8a2df2332a2febc2dd8dc8d0db250b63a5586a5
Miguel Ángel Sicilia GarcÍa <m.sicilia.garcia@alumnos.upm.es>
2025-05-05 18:42:51 +0200

    Fixed api information endpoint and added test execution to client

--- patient_app/src/client.py
+++ patient_app/src/client.py
@@ -23,18 +23,46 @@ def get_prediction(X_new, client=client):
         "http://127.0.0.1:5001/predict",
         files=files
     )
-
+    if encrypted_response.status_code != 200:
+        raise ValueError(f"Error in prediction request: {encrypted_response.status_code} - {encrypted_response.text}")
+    if encrypted_response.headers.get('Content-Type') != 'text/plain':
+        raise ValueError(f"Unexpected response format: {encrypted_response.headers.get('Content-Type')}")
+    
+    if not encrypted_response.content:
+        raise ValueError("Empty response from server.")
+    
     return client.deserialize_decrypt_dequantize(encrypted_response.content)
 
+#TODO revisar esta funcion, igual hay partes innecesarias
 def get_server_info():
     response = requests.get("http://127.0.0.1:5001/info")
     if response.status_code == 200:
-        info = response.json()
-        required_features = info.get("omop_requirements", {}).get("required_measurements_by_source_value", [])
-        expected_format = info.get("omop_requirements", {}).get("expected_input_format", {})
-        return {
-            "required_features": required_features,
-            "expected_format": expected_format
-        }
+        metadata = response.json()
+        if all(key in metadata for key in ["omop_requirements", "fhe_evaluation_keys", 
+                                           "prediction_endpoint", "prediction_method"]):
+            return metadata
+        else:
+            raise ValueError("Unexpected response format from server.")
     else:
         response.raise_for_status()
+
+if __name__ == "__main__":
+
+    info = get_server_info()
+    required_features = info["omop_requirements"]["required_measurements_by_source_value"]
+    n_features = len(required_features)
+    
+    # TODO In a real scenario, these values should be replaced with actual data that should be extracted from the OMOP database.
+    X_new = np.array([[13.54, 14.36, 87.46, 566.3, 0.09779, 0.08129, 0.06664, 0.04781, 0.1885, 0.05766,
+                       0.2699, 0.7886, 2.058, 23.56, 0.008462, 0.01460, 0.02387, 0.01315, 0.01980, 0.002300,
+                       15.11, 19.26, 99.70, 711.2, 0.14400, 0.17730, 0.23900, 0.12880, 0.2977, 0.07259]])
+    
+    print(f"Created test data with {n_features} features:")
+    for feat, val in zip(required_features, X_new[0]):
+        print(f"{feat}: {val:.3f}")
+
+    if X_new.size > 0:
+        prediction = get_prediction(X_new)
+        print("Decrypted prediction:", prediction)
+    else:
+        print("No input data available for prediction.")
--- provider/model_api.py
+++ provider/model_api.py
@@ -32,17 +32,45 @@ def get_info():
                 "radius1",
                 "texture1",
                 "perimeter1",
-                #TODO add the rest of the params
+                "area1",
+                "smoothness1",
+                "compactness1",
+                "concavity1",
+                "concave_points1",
+                "symmetry1",
+                "fractal_dimension1",
+                "radius2",
+                "texture2",
+                "perimeter2",
+                "area2",
+                "smoothness2",
+                "compactness2",
+                "concavity2",
+                "concave_points2",
+                "symmetry2",
+                "fractal_dimension2",
+                "radius3",
+                "texture3",
+                "perimeter3",
+                "area3",
+                "smoothness3",
+                "compactness3",
+                "concavity3",
+                "concave_points3",
+                "symmetry3",
                 "fractal_dimension3"
             ],
             "query_hint": "Retrieve measurements where measurement_concept_id = 0 and measurement_source_value is in the list above.",
-            "expected_input_format": { #TODO darle una vuelta a esto
+            "expected_input_format": {
                 "structure": "object_with_key_value_pairs",
                 "key": "measurement_source_value",
                 "value": "value_as_number"
             }
         },
-        #TODO add the fhe evaluation keys
+        "fhe_evaluation_keys":{
+            "key1": "value1",
+            "key2": "value2"
+        },
         "prediction_endpoint": "/predict",
         "prediction_method": "POST"
     }
@@ -52,17 +80,18 @@ def get_info():
 def predict():
     """Receives encrypted data and evaluation keys, returns encrypted prediction."""
     try:
-        encrypted_data = request.data
-        serialized_evaluation_keys = request.headers.get('Evaluation-Keys')
+        encrypted_data_file = request.files.get('encrypted_data')
+        evaluation_keys_file = request.files.get('evaluation_keys')
 
-        if not encrypted_data:
-            return jsonify({"error": "Missing encrypted data in request body"}), 400
-        if not serialized_evaluation_keys:
-            return jsonify({"error": "Missing 'Evaluation-Keys' header"}), 400
+        if not encrypted_data_file or not evaluation_keys_file:
+            return "Missing files in the request", 400
+
+        encrypted_data = encrypted_data_file.read()
+        serialized_evaluation_keys = evaluation_keys_file.read()
 
         encrypted_result = server.run(encrypted_data, serialized_evaluation_keys)
 
-        return encrypted_result
+        return encrypted_result, 200, {'Content-Type': 'text/plain'}
 
     except Exception as e:
         app.logger.error(f"Prediction error: {e}", exc_info=True)
