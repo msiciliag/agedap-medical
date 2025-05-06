6e89b6f983978682e6709b88ce3f7227392d4bca
msiciliag <->
2025-05-06 19:36:39 +0200

    Refactor and minor fixes

--- provider/services/diabetes_classification.py
+++ provider/services/diabetes_classification.py
@@ -42,4 +42,4 @@ class DiabetesClassification(AIServiceEndpoint):
 
 if __name__ == "__main__":
     app = DiabetesClassification().app
-    app.run(host='0.0.0.0', port=5001, debug=False)
+    app.run(host='0.0.0.0', port=5002, debug=False)
